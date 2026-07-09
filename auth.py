166b5916891116b9bc84fe2612c81f7aacd30cc8
Wilfredo Rodriguez <wilfredo319401@gmail.com>
2026-07-09 00:27:41 +0000

    Added Password Strength Levels

--- auth.py
+++ auth.py
@@ -3,34 +3,51 @@ from main import User, CheckEmailValidity, CheckPasswordStrength
 
 registered_users = []
 
-def find_user_by_email(email):
+def findUserByEmail(email):
     for user in registered_users:
         if user.email == email:
             return user
     return None
 
-def register_user(name, email, password):
+def getPasswordStrengthLevel(password):
+    score = CheckPasswordStrength(password)
+    
+    if score< 1:
+        score = 1
+    if score > 4:
+        score = 4
+    levels = {
+        1: "Very Weak",
+        2: "Weak",
+        3: "Strong",
+        4: "Very Strong"
+    }
+    return score, levels[score]
+
+def registerUser(name, email, password):
     if not CheckEmailValidity(email):
         return False, "Invalid email format"
-    if not CheckPasswordStrength(password):
-        return False, "Password must be at least 6 characters"
-    if find_user_by_email(email) is not None:
+    passwordScore, passwordLevel = getPasswordStrengthLevel(password)
+    if passwordScore < 3:
+        return False, f"Password is too weak. Strength: {passwordLevel}"
+    if findUserByEmail(email) is not None:
         return False, "Email is already registered"
     
-    hashed_password = generate_password_hash(password)
+    hashedPassword = generate_password_hash(password)
     
-    new_user = User(name, email, hashed_password)
+    new_user = User(name, email, hashedPassword)
     registered_users.append(new_user)
     return True, "User registered successfully"
 
-def login_user(email,password):
-    user = find_user_by_email(email)
+def loginUser(email,password):
+    user = findUserByEmail(email)
     
     if user is None:
         return False, "User not found"
+    
     if not check_password_hash(user.password, password):
         return False, "Incorrect password"
     return True, "Login successful"
 
-def logout_user():
+def logoutUser():
     return True, "User logged out successfully"
