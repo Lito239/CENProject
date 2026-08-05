671156abe570abd0f9fb063102b912c0c66b3a8f
mrjacob8282 <117129463+mrjacob8282@users.noreply.github.com>
2026-08-04 19:43:48 -0500

    Update auth.py
    Static code Changes

--- auth.py
+++ auth.py
@@ -1,18 +1,24 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from main import User, CheckEmailValidity, CheckPasswordStrength
-from database import PythonDatabase
+"""
+The authorization module for CurrencyCare.
 
+This module manages every authorization when loggin into currency care.
+"""
+from werkzeug.security import generate_password_hash, check_password_hash
+from main import CheckEmailValidity, CheckPasswordStrength
+from database import PythonDatabase
 database = PythonDatabase()
 
 def findUserByEmail(email):
+    """Find a user by email."""
     return database.get_user_by_email(email)
 
 def findUserByUsername(username):
+    """Find a user by username."""
     return database.get_user_by_username(username)
 
 def getPasswordStrengthLevel(password):
+    """Get password strength level."""
     score = CheckPasswordStrength(password)
-    
     if score< 1:
         score = 1
     if score > 4:
@@ -26,19 +32,19 @@ def getPasswordStrengthLevel(password):
     return score, levels[score]
 
 def registerUser(name, email, password):
+    """Register a new user."""
     if not CheckEmailValidity(email):
         return False, "Invalid email format"
-    passwordScore, passwordLevel = getPasswordStrengthLevel(password)
-    if passwordScore < 3:
-        return False, f"Password is too weak. Strength: {passwordLevel}"
+    password_score, password_level = getPasswordStrengthLevel(password)
+    if password_score < 3:
+        return False, f"Password is too weak. Strength: {password_level}"
     if findUserByEmail(email) is not None:
         return False, "Email is already registered"
-    
-    hashedPassword = generate_password_hash(password)
+    hashed_password = generate_password_hash(password)
     result = database.add_user(
         username=name,
         email=email,
-        password=hashedPassword
+        password=hashed_password
     )
     if result == "username already exists":
         return False, "Username is already registered"
@@ -46,15 +52,15 @@ def registerUser(name, email, password):
         return False, "Email is already registered"
     return True, "User registered successfully"
 
-def loginUser(userName, password):
-    user = findUserByUsername(userName)
-    
+def loginUser(user_name, password):
+    """Login a user."""
+    user = findUserByUsername(user_name)
     if user is None:
         return False, "User not found"
-    
     if not check_password_hash(user["password"], password):
         return False, "Incorrect password"
     return True, "Login successful"
 
 def logoutUser():
+    """Logout a user."""
     return True, "User logged out successfully"
